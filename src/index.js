b2d954d2dd0aca9ab590bdc6fceecc1c56e64cc7
Roma Ovcharenko <prog3@gerchikco.com>
2021-09-06 22:49:09 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,50 +1,31 @@
-const createImage = num => {
-  const img = new Image();
-  img.classList.add("card__image");
-  img.loading = "lazy";
-  img.src = `./img/pexels-${ num }.jpeg`;
+const createEl = el => document.createElement(el);
 
-  img.addEventListener("load", e => {
-    const url = new URL(e.target.src);
-    console.log(`finished load ${ url.pathname }`)
-  })
-
-  img.addEventListener("error", e => {
-    const url = new URL(e.target.src);
-    console.log(`failed load ${ url.pathname }`)
-  })
-
-  return img;
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM CONTENT LOADED done");
-
-  const images = document.querySelectorAll("img");
-
-  Array.from(images, image => {
-    image.addEventListener("load", e => {
-      console.log(`loaded image ${ e.target.src }`);
-    })
-  })
-
+document.addEventListener("DOMContentLoaded", () => {  
   const main = document.querySelector("main");
 
-  for (let i = 7; i<= 40; i++) {
-    const article = document.createElement("article");
-
-    const p = document.createElement("p");
-    p.textContent = `Lorem ipsum dolor sit, 
-                     amet consectetur adipisicing elit. 
-                     Deleniti, vero?`;
+  for (let i = 7; i<= 12; i++) {
+    const article = createEl("article");
     
-    article.classList.add("card");
-    article.append(createImage(i), p);
-    main.append(article);
+    const p = createEl("p");
+    p.textContent = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, vero?`;
+    
+
+    console.log(i);
+    const img = new Image();
+    img.classList.add("card__image");
+    img.setAttribute("loading", "lazy");
+    img.setAttribute("src", `./img/pexels-low-${ i }.jpeg`);
+    img.addEventListener("load", (e) => {
+      console.log(111);
+      // let i = new Image();
+      // i.classList.add("card__image");
+      // i.setAttribute("src", img.src.replace("-low-", "-"));
+      // i.setAttribute("decoding", "async");
+      
+    })   
+    
+    // article.classList.add("card");
+    // article.append(img, p);
+    // main.append(article);
   }
-})
-
-
-window.addEventListener("load", () => {
-  console.log("WINDOW LOAD done")
 })
